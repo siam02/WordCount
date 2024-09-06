55f965bbcc026dae3b327e20ba320913d883ed7c
nowshadjaman21 <nowshad.nirob@gmail.com>
2024-09-06 23:59:39 +0600

    new style

--- WordCount.js
+++ WordCount.js
@@ -1,19 +1,26 @@
+
+const TextArea = document.getElementById("text");
+
+TextArea.addEventListener("input", wordCount);
+
+
 function wordCount() {
 
     const Textarea = document.getElementById("text").value;
+    
     const wordCount = Textarea.trim().split(/\s+/).filter(word => word.length > 0).length;
     const charCount = Textarea.length;
-    const sentenceCount = Textarea.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0).length;
+    // const sentenceCount = Textarea.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0).length;
 
 
     document.getElementById("word").textContent = wordCount;
     document.getElementById("char").textContent = charCount;
-    document.getElementById("sentence").textContent = sentenceCount;
+    // document.getElementById("sentence").textContent = sentenceCount;
 }
 
-function ClearText() {
-    document.getElementById("text").value = "";
-    document.getElementById("word").textContent = "";
-    document.getElementById("char").textContent = "";
-    document.getElementById("sentence").textContent = "";
-}
+// function ClearText() {
+//     document.getElementById("text").value = "";
+//     document.getElementById("word").textContent = "";
+//     document.getElementById("char").textContent = "";
+//     document.getElementById("sentence").textContent = "";
+// }
